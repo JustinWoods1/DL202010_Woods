95fed6bbf473c44e1bb4d677ae5a5004695897b0
JustinWoods1 <59978749+JustinWoods1@users.noreply.github.com>
2020-04-10 11:26:24 -0500

    lab10
    Changed 'clock' input to 'clk'.
    Still errors

--- Lab 10/Lab 10/Lab 10.srcs/sources_1/new/calc_lab10.sv
+++ Lab 10/Lab 10/Lab 10.srcs/sources_1/new/calc_lab10.sv
@@ -1,8 +1,8 @@
 
 
 module calc_lab10(
-    input btnU, btnD, clock, btnC,
-    input [15:0]sw,
+    input btnU, btnD, clk, btnC,
+    input [15:0] sw,
     output [6:0] seg,
     output [3:0] an,
     output [15:0] led,
@@ -11,14 +11,14 @@ module calc_lab10(
     
        
     top_lab9 calc_unit(
-        .btnU(btnU), .btnD(btnD), .clk(clock), .btnC(btnC),
+        .btnU(btnU), .btnD(btnD), .clk(clk), .btnC(btnC),
         .sw(sw),
         .led(led)
     );
     
     sseg4_TDM disp_unit(
         .data({8'b00000000, led[15:8]}), .hex_dec(sw[15]),
-        .sign(sw[14]), .reset(btnC), .clock(clock),
+        .sign(sw[14]), .reset(btnC), .clock(clk),
         .seg(seg), .an(an), .dp(dp)
         
     );
